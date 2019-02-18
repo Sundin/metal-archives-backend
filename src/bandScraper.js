50570c4fbfdb49bc9eeb0a8b72ef12aa226caf9f
Gustav Sundin <sundin666@gmail.com>
2019-02-18 12:41:34 +0100

    Cleanup scraper

--- src/bandScraper.js
+++ src/bandScraper.js
@@ -6,9 +6,9 @@ const request = require('request-promise-native');
 const logger = require('./util/logger.js');
 
 module.exports = {
-    scrapeBandPage: (bandName, id) => {
+    scrapeBandPage: (id) => {
         return new Promise((resolve, reject) => {
-            const url = 'https://www.metal-archives.com/bands/' + bandName + '/' + id;
+            const url = 'https://www.metal-archives.com/band/view/id/' + id;
             logger.info('Scraping ' + url);
 
             request.get(url).then(body => {
@@ -16,13 +16,11 @@ module.exports = {
                 const bandName = $('h1[class=band_name]').text();
 
                 const bandUrl = $('h1[class=band_name] a').attr('href');
-                const splitUrl = bandUrl.split('/');
-                const id = splitUrl[splitUrl.length - 1];
 
                 const photoUrl = $('a[id=photo]').attr('href');
-                const logoUrl = $('a[id=photo]').attr('href');
+                const logoUrl = $('a[id=logo]').attr('href');
 
-                let band_values = {};
+                let bandValues = {};
 
                 const bandKeys = [
                     'country',
@@ -37,23 +35,23 @@ module.exports = {
 
                 $('div#band_stats dd').each(function(i, elem) {
                     if (i === 6) {
-                        const label_url = $(elem).find('a').attr('href');
-                        let label_id = '';
-                        if (label_url) {
-                            const splitted_url = label_url.split('/');
-                            const split_again = splitted_url[splitted_url.length - 1].split('#');
-                            label_id = split_again[0];
+                        const labelUrl = $(elem).find('a').attr('href');
+                        let labelId = '';
+                        if (labelUrl) {
+                            const splittedUrl = labelUrl.split('/');
+                            const splitAgain = splittedUrl[splittedUrl.length - 1].split('#');
+                            labelId = splitAgain[0];
                         }
 
                         const label = {
-                            _id: label_id,
+                            _id: labelId,
                             name: $(elem).text().trim(),
-                            url: label_url
+                            url: labelUrl
                         };
-                        band_values.label = label;
+                        bandValues.label = label;
                     } else {
                         // TODO: trim tabs in middle of string
-                        band_values[bandKeys[i]] = $(elem).text().trim();
+                        bandValues[bandKeys[i]] = $(elem).text().trim();
                     }
                 });
 
@@ -65,18 +63,19 @@ module.exports = {
                     live: getMembers(body, 'live')
                 };
 
+                // TODO: make network requests in parallel
                 getDiscography(id).then(discography => {
                     const bandData = {
                         band_name: bandName,
                         _id: id,
-                        country: band_values.country,
-                        location: band_values.location,
-                        status: band_values.status,
-                        formed_in: band_values.active_since,
-                        genre: band_values.genre,
-                        themes: band_values.themes,
-                        label: band_values.label,
-                        years_active: band_values.years_active,
+                        country: bandValues.country,
+                        location: bandValues.location,
+                        status: bandValues.status,
+                        formed_in: bandValues.active_since,
+                        genre: bandValues.genre,
+                        themes: bandValues.themes,
+                        label: bandValues.label,
+                        years_active: bandValues.years_active,
                         url: bandUrl,
                         photo_url: photoUrl,
                         logo_url: logoUrl,
@@ -107,7 +106,7 @@ function getDiscography(bandId) {
         request.get(url).then(body => {
             const $ = cheerio.load(body);
 
-            const discog_keys = [
+            const discKeys = [
                 'name',
                 'type',
                 'year',
@@ -117,11 +116,11 @@ function getDiscography(bandId) {
             $('tbody tr').each(function(i, album) {
                 let disc = {};
                 $(album).find('td').each(function(j, item) {
-                    disc[discog_keys[j]] = $(item).text().trim(); // TODO: trim tabs inside text?
+                    disc[discKeys[j]] = $(item).text().trim(); // TODO: trim tabs inside text?
                 });
                 const albumUrl = $(album).find('a').attr('href');
-                const splitted_url = albumUrl.split('/');
-                const id = splitted_url[splitted_url.length - 1];
+                const splittedUrl = albumUrl.split('/');
+                const id = splittedUrl[splittedUrl.length - 1];
 
                 discography.push({
                     _id: id,
@@ -141,19 +140,19 @@ function getDiscography(bandId) {
 function getMembers(page, type) {
     let members = [];
     let member = {};
-    const member_keys = [
+    const memberKeys = [
         'name',
         'instrument'
     ];
 
     const $ = cheerio.load(page);
-    $(`div#band_tab_members_${type} div table tr.lineupRow`).each(function(i, member_data) {
-        $(member_data).find('td').each(function(j, item) {
-            member[member_keys[(j + 2) % 2]] = $(item).text().trim(); // TODO: trim middle of string?
+    $(`div#band_tab_members_${type} div table tr.lineupRow`).each(function(i, memberData) {
+        $(memberData).find('td').each(function(j, item) {
+            member[memberKeys[(j + 2) % 2]] = $(item).text().trim(); // TODO: trim middle of string?
             if ((j + 2) % 2 === 0) {
                 const url = $(item).find('a').attr('href');
-                const splitted_url = url.split('/');
-                member._id = splitted_url[splitted_url.length - 1];
+                const splittedUrl = url.split('/');
+                member._id = splittedUrl[splittedUrl.length - 1];
                 member.url = url;
             } else if ((j + 2) % 2 === 1) {
                 members.push(member);
@@ -161,23 +160,23 @@ function getMembers(page, type) {
             }
         });
 
-        const next_tag = $(member_data).next();
+        const nextTag = $(memberData).next();
 
-        if (next_tag && $(next_tag).hasClass('lineupBandsRow')) {
-            $(next_tag).find('td').each(function(k, item) {
-                let also_array = [];
+        if (nextTag && $(nextTag).hasClass('lineupBandsRow')) {
+            $(nextTag).find('td').each(function(k, item) {
+                let otherBands = [];
                 $(item).find('a').each(function(j, also) {
-                    let other_band = {};
-                    other_band.band_name = $(also).text().trim(); // TODO: trim middle of string?
+                    let band = {};
+                    band.band_name = $(also).text().trim(); // TODO: trim middle of string?
                     const url = $(also).attr('href');
-                    const splitted_url = url.split('/');
-                    other_band._id = splitted_url[splitted_url.length - 1];
+                    const splittedUrl = url.split('/');
+                    band._id = splittedUrl[splittedUrl.length - 1];
 
                     const pastMember = $(also).prev().prevObject[0].prev.data.includes('ex-');
-                    other_band.still_member = !pastMember;
-                    also_array.push(other_band);
+                    band.still_member = !pastMember;
+                    otherBands.push(band);
                 });
-                members[i].see_also = also_array;
+                members[i].see_also = otherBands;
             });
         }
     });
